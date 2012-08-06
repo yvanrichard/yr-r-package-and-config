309a37963c0441b0b385aa4226207599cc966210
yvanrichard <yvan@dragonfly.co.nz>
2012-08-06 16:15:21 +1200

    Improved check_cited_labels to consider cross-referencing between files

--- myfunctions.r
+++ myfunctions.r
@@ -912,6 +912,7 @@ get_in_out_from_scripts <- function(fold='.')
 ## "%nin%" <- function(x,y) !(x %in% y)
 
 ## Check labels in LaTeX report that are not cited in text
+## reportfile='~/dragonfly/sra-foundations/report/notes/report.tex'
 check_cited_labels <- function(reportfile, ignore=c('sec','eq'))
   {
     if (!grepl('^/|^~', reportfile))  reportfile <- sprintf('%s/%s',getwd(),reportfile)
@@ -920,32 +921,60 @@ check_cited_labels <- function(reportfile, ignore=c('sec','eq'))
     txt <- txt[!comments]
     inputs <- grep('\\\\input\\{', txt, value=T)
     inputs <- gsub('^ *\\\\input\\{(.*)\\}', '\\1', inputs)
+    alltext <- NULL
+    alllabels <- NULL
     f=inputs[4]
     for (f in inputs)
       {
         file <- f
         if (!grepl('^/|^~', file))  file <- sprintf('%s/%s',getwd(), file)
         file <- sprintf('%s.tex', file)
-        cat(sprintf('\n---  %s  ---\n', file))
         txt <- readLines(file)
         comments <- grepl('^ *%', txt) | txt==''
         txt <- txt[!comments]
+        alltext[[f]] <- txt
         labels0 <- grep('\\\\label\\{',txt)
         labels <- gsub('^ *\\\\label\\{(.*)\\}', '\\1', txt[labels0])
         c <- sapply(strsplit(labels,':'), function(x) x[1]) %in% ignore
         labels <- labels[!c]
-        i=1
-        if (length(labels))
-          {
-            for (i in 1:length(labels))
-              {
-                l <- labels[i]
-                l0 <- labels0[i]
-                gs <- grep(l, txt, fixed=T)
-                g <- gs[!(gs %in% l0)]
-                if (!length(g))
-                  cat(sprintf('Not cited in text:  %s\n', l))
-              }
-          }
+        alllabels[[f]] <- labels
       }
+    l <- unlist(alllabels)[1]
+    for (l in unlist(alllabels))
+      {
+        wh <- grep(l, unlist(alltext), value=T)
+        c <- grepl('\\\\label\\{', wh)
+        cited <- wh[!c]
+        if (!length(cited))
+          cat(sprintf(
+                sprintf('Not referenced: %%%is  in  %%s\n', max(nchar(unlist(alllabels)))),
+                      l, names(alltext[grep(sprintf('\\\\label\\{%s\\}', l), alltext)])))
+      }
+    ## for (f in inputs)
+    ##   {
+    ##     file <- f
+    ##     if (!grepl('^/|^~', file))  file <- sprintf('%s/%s',getwd(), file)
+    ##     file <- sprintf('%s.tex', file)
+    ##     cat(sprintf('\n---  %s  ---\n', file))
+    ##     txt <- readLines(file)
+    ##     comments <- grepl('^ *%', txt) | txt==''
+    ##     txt <- txt[!comments]
+    ##     labels0 <- grep('\\\\label\\{',txt)
+    ##     labels <- gsub('^ *\\\\label\\{(.*)\\}', '\\1', txt[labels0])
+    ##     c <- sapply(strsplit(labels,':'), function(x) x[1]) %in% ignore
+    ##     labels <- labels[!c]
+    ##     i=1
+    ##     if (length(labels))
+    ##       {
+    ##         for (i in 1:length(labels))
+    ##           {
+    ##             l <- labels[i]
+    ##             l0 <- labels0[i]
+    ##             gs <- grep(l, txt, fixed=T)
+    ##             g <- gs[!(gs %in% l0)]
+    ##             if (!length(g))
+    ##               cat(sprintf('Not cited in text:  %s\n', l))
+    ##           }
+    ##       }
+    ##   }
   }
